16cd8739f83f314646d6700d926dbe890b15f159
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-12-30 00:09:29 +0900

    add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path")
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+module.exports = {
+    entry: "./src/js/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "./dist"),
+        clean: true
+    },
+    devtool: "source-map",
+    mode: "development",
+    optimization: {
+        minimizer: [new TerserWebpackPlugin()]
+    }
+
+}
