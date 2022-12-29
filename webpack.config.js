5341f5037e21bac3d880088674765704538165e2
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-12-30 00:49:04 +0900

    add devServer

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,12 @@ module.exports = {
     },
     devtool: "source-map",
     mode: "development",
+    devServer: {
+        host: "localhost",
+        port: 8080,
+        open: true,
+        watchFiles: "index.html",
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: "keyboard",
