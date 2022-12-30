15caf44f290fc5776878f48ecac601d21247e3b6
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-12-30 17:43:16 +0900

    apply selected fontFamily

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,5 +1,6 @@
 export class Keyboard {
     #switchEl;
+    #fontSelectEl;
     constructor() {
         this.#assignElement();
         this.#addEvent();
@@ -7,6 +8,7 @@ export class Keyboard {
 
     #assignElement() {
         this.#switchEl = document.getElementById("switch");
+        this.#fontSelectEl = document.getElementById("font");
     }
     #addEvent() {
         this.#switchEl.addEventListener("change", (event) => {
@@ -15,5 +17,9 @@ export class Keyboard {
                 event.target.checked ? "dark-mode" : ""
             );
         });
+        this.#fontSelectEl.addEventListener("change", (event) => {
+            document.body.style.fontFamily = event.target.value
+
+        })
     }
 }
